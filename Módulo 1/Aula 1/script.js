a7082d6b1bd8d28a5a1951221d478e9eb3218d66
Darlan Carvalho <92957575+DarlanSC1712@users.noreply.github.com>
2021-12-26 21:21:13 -0300

    Add files via upload

---
+++ Módulo 1/Aula 1/script.js
@@ -0,0 +1,11 @@
+
+var nome = "Darlan";
+var idade = 19;
+var sobrenome = "Carvalho";
+
+window.alert("O meu nome é " + nome + " " + sobrenome + " " + "e eu tenho " + idade + " " + "anos")
+
+var idade = prompt("Digite sua idade")
+
+document.write("<h1>Aprendendo Javascript</h1>")
+
